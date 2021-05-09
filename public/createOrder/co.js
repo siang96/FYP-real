884189d38ce3cfe66d8edc71bddd80c52f898344
Siang96 <luckyguy.siang@gmail.com>
2021-05-09 11:43:05 +0800

    form ui

--- public/createOrder/co.js
+++ public/createOrder/co.js
@@ -1,31 +1,37 @@
 import { initSession } from "../sessionManager.js";
 import { initFire } from "../sharedFunction.js";
 import { customAlphabet } from "../assets/frameworks/nanoid.js";
-
+$.ajaxSetup ({
+  // Disable caching of AJAX responses
+  cache: false
+});
 const charGen =
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const nanoid = customAlphabet(charGen, 10);
-const orderid=nanoid();
+const orderid = customAlphabet(charGen, 10);
+//generates id orderid();
 $(document).ready(function () {
   initFire();
   initSession();
-  //load form auto input
-
   main();
 });
 
 function main() {
+  $("#submitButton").hide();
   $("#formOption").on("change", function () {
     var selectedForm = this.value;
+    $("#submitButton").show();
+    $("#personalInfoArea").load("../assets/html/personalInfo.html");
     if (selectedForm.match(/Flyers/gi)) {
-      $("#formArea").load("../assets/html/flyer.html");
+      $("#orderAddInfoArea").load("../assets/html/flyer.html");
     }
     if (selectedForm.match(/Hardcover/gi)) {
-      $("#formArea").load("../assets/html/hardcover.html");
+      $("#orderAddInfoArea").load("../assets/html/hardcover.html");
     }
-    if (selectedForm.match(/Stamp/gi)) {
-      $("#formArea").load("../assets/html/nameTag.html");
+    if (selectedForm.match(/businessCard/gi)) {
+      $("#orderAddInfoArea").load("../assets/html/businessCard.html");
+    }
+    if (selectedForm.match(/banner/gi)) {
+      $("#orderAddInfoArea").load("../assets/html/bunting.html");
     }
   });
-  
 }
